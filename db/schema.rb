49622dc7c8780178b444171c8e7ba872a50d2899
Nicolas S. Navarro <nicolassnavarro12@gmail.com>
2024-06-21 10:33:36 -0300

    Feat: Devise user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_21_131358) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_21_132811) do
   create_table "bars", force: :cascade do |t|
     t.string "name", null: false
     t.string "address", null: false
@@ -18,6 +18,23 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_21_131358) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
